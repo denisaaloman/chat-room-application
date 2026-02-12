f48772fbb4503965fa30e74539157750ca4d8c5d
denisaaloman <denisa.aloman@stud.ubbcluj.ro>
2026-02-12 18:55:16 +0200

    commit

---
+++ src/main/java/com/chatapp/controller/ChatController.java
@@ -0,0 +1,43 @@
+package com.chatapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import com.chatapp.model.ApplicationStats;
+import com.chatapp.model.Message;
+import com.chatapp.utils.ApplicationUtils;
+
+@Controller
+public class ChatController {
+
+	@Autowired
+	SimpMessagingTemplate template;
+
+	@MessageMapping("/chat.message")
+	@SendTo("/topic/public")
+	public Message sendMessage(@Payload Message message) {
+		// Set the message time as now before sending it back to the topic
+		message.setTime(ApplicationUtils.getTime());
+		return message;
+	}
+
+	@MessageMapping("/chat.user")
+	@SendTo("/topic/public")
+	public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", message.getUser());
+		message.setTime(ApplicationUtils.getTime());
+		return message;
+	}
+
+	@Scheduled(fixedDelay = 1000)
+	public void sendAdhocMessage() {
+		// Send the updated user count to the chat.
+		template.convertAndSend("/topic/stats", ApplicationStats.getUserCount());
+	}
+}
---
+++ src/test/java/com/chatapp/ApplicationStatsTest.java
@@ -0,0 +1,44 @@
+package com.chatapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.chatapp.model.ApplicationStats;
+
+import java.util.stream.IntStream;
+
+public class ApplicationStatsTest {
+
+	@Test
+	public void test_incrementUserCount() throws InterruptedException {
+		IntStream stream1 = IntStream.range(0, 5000);
+		IntStream stream2 = IntStream.range(0, 5000);
+
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				stream1.forEach(i -> {
+					ApplicationStats.incrementUserCount();
+				});
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				stream2.forEach(i -> {
+					ApplicationStats.decrementUserCount();
+				});
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+
+		Assertions.assertEquals(0, ApplicationStats.getUserCount());
+	}
+
+}
